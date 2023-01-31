13e920af422091ef91762150f8374fff05e4de29
sherchryst <sherchryst@icloud.com>
2023-01-31 18:25:08 +0100

    Implemented new features for improved image display
    -Reduced image size for improved performance
    Adapted design for desktop display with added elements per line
    -Made top bar scrollable for better visibility in panorama mode
    -Added support for pagination in search method to display more images
    This commit brings a series of improvements to enhance the user experience for viewing images in the application.

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -16,39 +16,48 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   final TextEditingController _searchController = TextEditingController();
   List<Photo>? _photos;
+  int currentPage = 1;
+  int totalPages = 0;
+  int perPage = 20;
 
   /// Search for photos with the text entered in the search bar
-  void _searchPhotos(BuildContext context) async {
+  void _searchPhotos(BuildContext context, bool isNextPage) async {
     // If the search bar is empty, we do not make the request
     if (_searchController.text.isEmpty) {
       return;
     }
-    // We make the request
+    // If we are loading the next page, we increment the current page,
+    // otherwise, we reset the current page to 1
+    setState(() {
+      currentPage = isNextPage ? currentPage + 1 : 1;
+      perPage = isNextPage ? 21 : 20;
+    });
+    // Make the API request to search for photos
     final response = await apiClient.searchPhotos(
       query: _searchController.text,
+      page: currentPage,
+      perPage: perPage,
     );
-    // We check the response status code
+    // Check if the response is successful (HTTP status code 200)
     if (response.statusCode == 200) {
       List<Photo> photos = [];
       List results = response.data["results"];
-      // We create a list of Photo objects from the results
+      // Create a list of `Photo` objects from the results
       for (var result in results) {
         photos.add(Photo.fromJson(result));
       }
       setState(() {
-        _photos = photos;
+        // If loading the next page, add the new photos to the existing list,
+        // otherwise, replace the entire list with the new photos
+        _photos = isNextPage ? [..._photos!, ...photos] : photos;
+        totalPages = response.data["total_pages"];
       });
     } else {
-      // If the response status code is not 200, we display an error message
+      // If the response is not successful, display an error message
       if (mounted) {
-        apiClient.displayMessage(context, "Une erreur est survenue", true);
+        apiClient.displayMessage(context, "An error occurred", true);
       }
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
   }
 
   @override
@@ -59,7 +68,8 @@ class _HomeViewState extends State<HomeView> {
           basics.vSpaceMedium,
           searchBar(context, _searchController, _searchPhotos),
           basics.vSpaceMedium,
-          resultsPhoto(_photos)
+          resultsPhoto(
+              context, _photos, _searchPhotos, currentPage < totalPages),
         ],
       ),
     );
